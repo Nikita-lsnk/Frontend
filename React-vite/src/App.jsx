07089a094b23ff29bb3d43dd816f04a838e72039
Nikita-lsnk <Nek14091998@gmail.com>
2023-02-18 17:27:39 +0300

    add login block and media request

--- React-vite/src/App.jsx
+++ React-vite/src/App.jsx
@@ -1,36 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.sass'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="login">
+        <div className="text login__text">
+          <h1 className="title text__title">Welcome.</h1>
+          <span className="subtitle text__subtitle">Hello, nice to se you again!</span>
+        </div>
+        <div className="board login__board">
+          <form action="#" method='get'></form>
+          <p className='title-field title-field__first'>email/username</p>
+          <input className='field field__name' type="text" name='username' />
+          <p className='title-field title-field__second'>password</p>
+          <input className='field field__pass' type="password" name='userpass' />
+          <button className='main-botton board__main-botton'>Login</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <h1>{count}</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Конечно!
-        </button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Еще как!
-        </button>
-        <p>
-          Я смогу наверстать все за неделю?
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Наведи на логотип и удивись как он светится!
-      </p>
     </div>
   )
 }
