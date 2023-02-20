dc6b5a918000fd400afc5dd878b710bf8163fe6d
Nikita-lsnk <Nek14091998@gmail.com>
2023-02-20 10:22:59 +0300

    fix media style

--- React-vite/src/App.jsx
+++ React-vite/src/App.jsx
@@ -12,14 +12,13 @@ function App() {
           <h1 className="title text__title">Welcome.</h1>
           <span className="subtitle text__subtitle">Hello, nice to se you again!</span>
         </div>
-        <div className="board login__board">
-          <form action="#" method='get'></form>
+        <form action="#" method='get' className="board login__board">
           <p className='title-field title-field__first'>email/username</p>
-          <input className='field field__name' type="text" name='username' />
+          <input className='field field__name' type="text" name='username' placeholder='@username'/>
           <p className='title-field title-field__second'>password</p>
-          <input className='field field__pass' type="password" name='userpass' />
-          <button className='main-botton board__main-botton'>Login</button>
-        </div>
+          <input className='field field__pass' type="password" name='userpass' placeholder='password'/>
+          <button type='submit' className='main-botton board__main-botton'>Login</button>
+        </form>
       </div>
     </div>
   )
