a790b1067a11c6110bf56db67e7ba8b5f95093d8
Nikita-lsnk <Nek14091998@gmail.com>
2023-03-01 21:47:09 +0300

    add axios and useState and useEffect in About

--- React-vite/src/components/About.jsx
+++ React-vite/src/components/About.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import styled from 'styled-components';
+
+const src ="https://content.guardianapis.com/search?page=2&q=debate&api-key=test"
+
+
 
 const About = () => {
+
+  const [articles, setArticles] = useState ([]);
+
+  useEffect (()=>{
+    axios
+    .get(src)
+    .then(data => {
+      setArticles(data.data.response.results);
+    })
+  }, []);
+
   return (
-    <div>About</div> 
+    <div>
+      {articles.map(article =>{
+        return (
+          <p><a href={article.webUrl}> {article.webTitle} </a></p>
+        );
+      })}     
+    </div> 
   )
 }
 
 export default About
+
